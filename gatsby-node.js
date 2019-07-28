ce2251aa946f5875351fc3c19c28f879e5bdfcad
Hector Carvajal <h_carvajal@outlook.es>
2019-07-28 15:28:26 -0500

    gatsby template settings into gatsby-nodejs

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,42 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require('path')
+
+exports.createPages = async ({graphql, actions}) => {
+    const { createPage } = actions 
+    const productTemplate = path.resolve(`src/templates/Product.js`)
+    const result = await graphql(`
+        query GET_SKUS{
+            allStripeSku {
+                edges {
+                    node {
+                        id
+                        currency
+                        price
+                        product {
+                            id
+                            name
+                            metadata {
+                                img
+                                description
+                                verdura
+                            }
+                        }
+                    }
+                }
+            }
+        }`)
+
+        if(result.errors){
+            throw result.errors
+        }
+        result.data.allStripeSku.edges.forEach(({node}) => {
+            createPage({
+                path: `${node.id}`,
+                component: productTemplate,
+            })
+        })
+    } 
+
